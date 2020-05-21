e2c06aae9ff3b677331c3f132fd6d7025b51740e
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-05-21 13:54:52 +0200

    editing routes and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthComponent } from './auth/auth.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'auth', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
