53989ee3d685fce72fe051cfc4fcfda4d012b723
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-05-24 05:21:48 +0200

    edit navbar

--- src/app/profile/profile-routing/profile-routing.module.ts
+++ src/app/profile/profile-routing/profile-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from '../profile.component';
 import { ProfileDetailsComponent } from '../profile-details/profile-details.component';
-import { ProfileFormComponent } from '../profile-form/profile-form.component';
 
 const routes: Routes = [
   { path: ':profileId', component: ProfileDetailsComponent },
-  { path: 'edit/:profileId', component: ProfileFormComponent },
 ];
 
 @NgModule({
