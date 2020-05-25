5d22d7b4ad012e8d9dcc5668d60bcdd2998962d0
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-05-25 09:27:25 +0200

    adding validations on updating name and email

--- src/app/profile/personal-form/personal-form.component.ts
+++ src/app/profile/personal-form/personal-form.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange  } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 import { UserService } from '../../service/user.service';
+import { Seeker } from '../../models/seeker';
 
 @Component({
   selector: 'app-personal-form',
   templateUrl: './personal-form.component.html',
   styleUrls: ['./personal-form.component.css']
 })
-export class PersonalFormComponent implements OnInit {
+export class PersonalFormComponent implements OnInit, OnChanges {
+  error: any;
+  @Input() seekerData: Seeker;
   seeker: FormGroup ;
 
   constructor(private userService: UserService) { }
@@ -20,13 +23,23 @@ export class PersonalFormComponent implements OnInit {
     });
   }
 
+  ngOnChanges(changes: {[propKey: string]: SimpleChange})
+  {
+      this.seeker? this.seeker.patchValue(this.seekerData) : null ;
+  }
+
   onClickSubmit(formData)
   {
     let user_id = this.userService.user_id;
     console.log(formData);
     console.log(user_id);
     this.userService.updateUser(user_id, formData)
-    .subscribe((data) => console.log(data));
+    .subscribe((data) => {
+      console.log("dataaaa",data)
+      this.error = data.message? data : null;
+      console.log("errorrrrrr", this.error);
+
+    });
 
   }
 }
