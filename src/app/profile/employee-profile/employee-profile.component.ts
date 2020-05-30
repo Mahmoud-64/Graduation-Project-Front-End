a83dc186b71cf8e71ab37f722fee846bddb5965f
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-05-30 08:10:38 +0200

    adding employee profile and organizing components

---
+++ src/app/profile/employee-profile/employee-profile.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../../service/employee.service';
+import { Employee } from '../../models/employee';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-employee-profile',
+  templateUrl: './employee-profile.component.html',
+  styleUrls: ['./employee-profile.component.css']
+})
+export class EmployeeProfileComponent implements OnInit {
+  employee: Employee =
+    {
+      name: "",
+      email: "",
+      password: "",
+      position: "",
+      branch: "",
+    };
+  role;
+  constructor(
+    private employeeService: EmployeeService,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.getEmployee(+params.get('profileId'));
+    });
+  }
+
+  getEmployee(employeeId): void {
+    this.employeeService.getEmployee(employeeId)
+      .subscribe(employee => {
+        this.employee = employee;
+      });
+  }
+
+
+}
