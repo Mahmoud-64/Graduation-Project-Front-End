d315d9dba84bb1df0c98f244105dbcecd4b079da
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-06-10 21:14:07 +0200

    edit employee route to be accessible to employees only

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -12,42 +12,43 @@ export class AuthGuard implements CanActivate, CanLoad {
   constructor(
     private router: Router,
     private userService: UserService
-  ){}
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-        if (!this.userService.loggedIn()) {
-            this.router.navigate(['login']);
-            return false;
-        }
+    if (!this.userService.loggedIn()) {
+      this.router.navigate(['login']);
+      return false;
+    }
 
-      return this.userService.getLoggedInUser().pipe(map(data=>{
-          const role = route.data.role as Role;
-          let userRole = this.userService.getUserRole();
-          if (role && !userRole) {
-              this.router.navigateByUrl('/error403');
-              return false;
-          }
-          return true;
-        }));
+    return this.userService.getLoggedInUser().pipe(map(data => {
+      const role = route.data.role as Role;
+      let userRole = this.userService.getUserRole();
+      if (role != userRole) {
+        this.router.navigateByUrl('/error403');
+        return false;
+      }
+      return true;
+    }));
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-      if (!this.userService.loggedIn()) {
-            this.router.navigateByUrl('/');
-          return false;
-      };
 
-      return this.userService.getLoggedInUser().pipe(map(user=>{
-        let userRole = this.userService.getUserRole();
-          const role = route.data.role as Role;
-          if (role!=userRole) {
-                this.router.navigateByUrl('/');
-              return false;
-          }
-          return true;
+    if (!this.userService.loggedIn()) {
+      this.router.navigateByUrl('/');
+      return false;
+    };
+
+    return this.userService.getLoggedInUser().pipe(map(user => {
+      let userRole = this.userService.getUserRole();
+      const role = route.data.role as Role;
+      if (role != userRole) {
+        this.router.navigateByUrl('/');
+        return false;
+      }
+      return true;
     }))
   }
 }
